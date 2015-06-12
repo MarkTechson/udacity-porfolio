ab98b6ea513dda456b1095fca47a488ee58ea284
MarkitoMagic <mark.thompson19@gmail.com>
2015-06-12 01:22:59 -0500

    Initial commit for the project.

---
+++ README.md
@@ -0,0 +1,7 @@
+#Project One: Build a Portfolio Site
+Project to create an online portfolio using HTML/CSS and Twitter's Bootstrap.
+
+#Sources
+Main image // http://adadevelopersacademy.org/images/heroes/home.jpg
+Classic Arcade // http://windows.wonderhowto.com/how-to/10-best-free-games-windows-8-app-store-right-now-0140507/
+Map // Screenshot of google maps I searched :)
---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+(function() {
+  $(document).ready(function() {
+    // capture the modal open event and fill in the data for the
+    // project that clicked the modal.
+    $('#projectModal').on('show.bs.modal', function (event) {
+      var modal = $(this);
+      var button = $(event.relatedTarget);
+      var projectTitle = button.data('project-title');
+      var projectDescription = button.data('project-description');
+      var projectImage = button.data('project-image');
+
+      modal.find('.modal-title').text(projectTitle);
+      modal.find('.project-description').text(projectDescription);
+      modal.find('.project-image').attr('src', projectImage);
+      modal.find('.project-image').attr('alt', projectTitle + ' image');
+    });
+  });
+})();
